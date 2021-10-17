058cd0a43175edc785173f41264a792d64f45144
MarcosAntfon <fs37158@gmail.com>
2021-10-17 19:29:47 -0300

    JavaConstrutores

---
+++ MaratonaJavaConstrutores/src/ConstrutoresTest/AnimesTest01.java
@@ -0,0 +1,14 @@
+package ConstrutoresTest;
+
+import ConstrutoresDominios.Animes;
+
+public class AnimesTest01 {
+ 
+	public static void main(String[] args) {
+		
+		Animes animes = new Animes("Pokemon XYZ", "SKY TV", 85,"Aventura");
+		animes.imprime();
+
+
+	}
+}
